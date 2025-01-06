aaa32d50889d412ed377d5b2ef9c5a4390a0cef3
yamoo9 <yamoo9@naver.com>
2025-01-06 12:38:46 +0900

    [수정] 주석 정리

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-// Node.js JavaScript Runtime
+/* -------------------------------------------------------------------------- */
+/* Node.js JavaScript 실행 환경(Runtime Environment)                            */
+/* -------------------------------------------------------------------------- */
 
 // CommonJS 진영 모듈 호출 방식
 // const http = require('node:http')
@@ -12,21 +14,26 @@ const HOSTNAME = 'localhost';
 const PORT = 4000;
 
 const server = http.createServer((request /* 요청 */, response /* 응답 */) => {
+
+  /* 요청(Request) -------------------------------------------------------------- */
   console.log(request.url);
   console.log(request.query);
 
+  /* 응답(Response) ------------------------------------------------------------- */
   response.end(/* html */ `
-        <!doctype html>
-        <html lang="ko-KR">
-            <head>
-                <meta charset="utf-8" />
-                <title>TypeScript 에센셜</title>
-            </head>
-            <body>
-                <h1>타입스크립트 에센셜</h1>
-            </body>
-        </html>
+    <!doctype html>
+    <html lang="ko-KR">
+      <head>
+        <meta charset="utf-8" />
+        <title>TypeScript 에센셜</title>
+        <meta name="viewport" content="width: device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <h1>타입스크립트 에센셜</h1>
+      </body>
+    </html>
     `);
+
 });
 
 server.listen(PORT, HOSTNAME, () => {
