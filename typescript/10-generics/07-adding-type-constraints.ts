8347453bebb1cb2045cdc49de2af0f3306b1a900
yamoo9 <yamoo9@naver.com>
2025-01-07 13:04:45 +0900

    [실습] 타입 제약 조건 추가

--- typescript/10-generics/07-adding-type-constraints.ts
+++ typescript/10-generics/07-adding-type-constraints.ts
@@ -6,14 +6,14 @@
 // --------------------------------------------------------------------------
 
 {
-  function combine<T, U>(o1: T, o2: U): T & U {
+  function combine<T extends object, A extends object>(o1: T, o2: A): T & A {
     return { ...o1, ...o2 };
   }
 
   // 객체가 아닌 타입이 combine 함수에 전달되더라도 TypeScript는 아무런 경고를 하지 않습니다.
   // 제네릭에 타입 제약 조건을 추가해 오류가 표시되도록 설정합니다.
-  combine('design', 'workflow');
-  combine(true, 101);
+  // combine('design', 'workflow');
+  // combine(true, 101);
 }
 
 {
@@ -23,7 +23,10 @@
 
   // 제네릭 타입 T는 length 속성을 포함하는 지 알 수 없으므로 TypeScript가 경고합니다.
   // 제네릭에 타입 제약 조건을 설정해 문제를 해결해봅니다.
-  function getLengthPow<T>(o: T, powNumber: number = 2): number {
+  function getLengthPow<T extends Lengthy>(
+    o: T,
+    powNumber: number = 2
+  ): number {
     return o.length ** powNumber;
   }
 
