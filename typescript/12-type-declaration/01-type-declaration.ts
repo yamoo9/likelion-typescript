496216643bdd34b85e67096b2c601fd2b8d55dcd
yamoo9 <yamoo9@naver.com>
2025-01-07 15:28:58 +0900

    [실습] TypeScript 타입 선언 파일 (*.d.ts)

--- typescript/12-type-declaration/01-type-declaration.ts
+++ typescript/12-type-declaration/01-type-declaration.ts
@@ -8,6 +8,9 @@
 // ------------------------------------------------------------------------------
 
 import _ from 'lodash';
+import axios, { AxiosResponse } from 'axios';
+
+console.log(_.shuffle([2, 3, 4, 5, 6]));
 
 // axios 라이브러리를 설치한 후, 라이브러리에 선언된 타입 파일(index.d.ts)을 참고합니다.
 // TypeScript는 설치된 라이브러리 package.json 파일 types 또는 typing 항목에 연결된 타입 선언 파일을 확인합니다.
@@ -21,4 +24,33 @@ import _ from 'lodash';
 
   // axios를 사용해 사용자 또는 포스트 데이터를 비동기 요청/응답 받도록 합니다.
   // axios 요청 시, 전달받을 데이터 타입을 정의하는 방법을 살펴봅니다.
+  const response = await axios.get<{}, AxiosResponse<User>, {}>(API.users);
+
+  console.log(response.data.username);
+
+  const dummyUser = {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  };
+
+  type User = typeof dummyUser;
 }
