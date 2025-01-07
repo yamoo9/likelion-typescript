34f373a340c53339996a35aacb37e39daeaca2e6
yamoo9 <yamoo9@naver.com>
2025-01-07 10:11:17 +0900

    [실습] 클래스 필드 구문에 타입 에너테이션

--- typescript/09-classes/02-class-fields.ts
+++ typescript/09-classes/02-class-fields.ts
@@ -2,7 +2,7 @@
 // 📌 클래스 필드 (Class Fields)
 // ⭐️ URL : https://bit.ly/3AdXi1I
 // ------------------------------------------------------------------------------
-// - 클래스 필드 구문을 사용해 인스터스 프로퍼티를 구성 또한 타입 에너테이션이 필요합니다.
+// - 클래스 필드 구문을 사용해 인스턴스 프로퍼티를 구성 또한 타입 에너테이션이 필요합니다.
 // ------------------------------------------------------------------------------
 
 // 클래스 필드 구문을 사용해 score 타입과 초깃값을 지정하고,
@@ -12,14 +12,24 @@
   class Player {
     nickname: string;
     role: string;
+    score: number;
 
-    constructor(nickname: string, role: string) {
+    constructor(nickname: string, role: string, score: number) {
       this.nickname = nickname;
       this.role = role;
+      this.score = score;
+    }
+
+    scoreUp(point: number): void {
+      this.score += point;
+    }
+
+    scoreDown(point: number): void {
+      this.score -= point;
     }
   }
 
-  const yamoo9 = new Player('yamoo9', '멘토');
+  const yamoo9 = new Player('yamoo9', '멘토', 10);
 
   yamoo9.scoreUp(20);
   console.log(yamoo9.score);
