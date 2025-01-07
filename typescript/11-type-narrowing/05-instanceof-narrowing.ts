3393a1c3f45d78d2393dbb4e71d5c08070a05dd3
yamoo9 <yamoo9@naver.com>
2025-01-07 14:47:10 +0900

    instanceof 연산자 타입 범위 축소

--- typescript/11-type-narrowing/05-instanceof-narrowing.ts
+++ typescript/11-type-narrowing/05-instanceof-narrowing.ts
@@ -9,8 +9,11 @@
 {
   // date 타입 중 Date 클래스의 인스턴스 여부를 판별하도록 타입 범위를 축소합니다.
   const printFullDate = (date: string | Date) => {
+    if (typeof date === 'string') {
+      return new Date(date).toLocaleString();
+    }
+
     return date.toLocaleString();
-    return new Date(date).toLocaleString();
   };
 
   printFullDate(new Date());
@@ -33,8 +36,12 @@
 
   // entity 타입이 어떤 클래스의 인스턴스인지 여부를 판별할 수 있도록 타입 범위를 축소합니다.
   function printName(entity: Employee | Company) {
-    return entity.name;
-    return entity.fullname;
+    if (entity instanceof Employee) {
+      return entity.fullname;
+    }
+    if (entity instanceof Company) {
+      return entity.name;
+    }
   }
 
   printName(new Employee('cikd3zlwj@9', 'yamoo9'));
