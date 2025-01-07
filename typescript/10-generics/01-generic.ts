6693da1b0b20dbe454dd82d786af51b0e21a0dfe
yamoo9 <yamoo9@naver.com>
2025-01-07 11:14:59 +0900

    [실습] 제네릭 (Generics)

--- typescript/10-generics/01-generic.ts
+++ typescript/10-generics/01-generic.ts
@@ -6,12 +6,16 @@
 // - 처음엔 문법이 생소해 이해하고 사용하는데 어려움이 있지만, 자주 사용되므로 학습이 필요합니다.
 // --------------------------------------------------------------------------
 
+// Array<string>
+// const element = document.querySelector<HTMLDivElement>('.container');
+// const element = document.querySelector('.container') as HTMLSpanElement;
+
 {
   // 명시적인 타입 에너테이션 (Type Annotation)
   // TypeScript 타입 추론 (Type Inference)
-  const numbers = [-2, 13, 9];
-  const colors = ['제네릭', '함수', '클래스'];
+  const numbers: Array<number> = [-2, 13, 9];
+  const colors: Array<string> = ['제네릭', '함수', '클래스'];
 
-  numbers.push('100');
-  colors.splice(1, 1, 100);
+  numbers.push(+'100');
+  colors.splice(1, 1, 100 + '');
 }
