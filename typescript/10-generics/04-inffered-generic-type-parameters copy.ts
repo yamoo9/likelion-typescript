09b55cb28b4b3fcdac5dd931f0c790b0e3e22b66
yamoo9 <yamoo9@naver.com>
2025-01-07 11:38:48 +0900

    [실습] 제네릭 타입 추론

--- typescript/10-generics/04-inffered-generic-type-parameters copy.ts
+++ typescript/10-generics/04-inffered-generic-type-parameters copy.ts
@@ -21,8 +21,10 @@
     };
   }
 
+  const queue = createQueue([4, 8, 12]);
+
   // 사용자가 직접 제네릭 타입을 설정해야 하는 경우
-  const formElement = document.querySelector('.from');
+  const formElement = document.querySelector<HTMLFormElement>('.from')!;
   formElement.addEventListener('submit', (e) => {
     e.preventDefault();
   });
