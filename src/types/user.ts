84b9a97499ca7ee25a994da94dec5abb84ff7ca2
yamoo9 <yamoo9@naver.com>
2025-01-08 14:23:43 +0900

    [실습] TypeScript 유틸리티 타입 Pick, Omit

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,11 @@
-interface User {
+export interface User {
   id: number;
   name: string;
   gender: string;
   age: number;
 }
+
+// export type RequestUser = Pick<User, 'age' | 'gender' | 'name'>;
+export type RequestUser = Omit<User, 'id'>;
 
 export default User;
