fcc5e9cff03ccd90d46fad73ef85511e460522e7
yamoo9 <yamoo9@naver.com>
2025-01-08 15:57:19 +0900

    [정리] 필요한 주석을 제외한 나머지 주석 삭제

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ import usersRouter from './routes/users';
 
 /* CONFIG. ------------------------------------------------------------------ */
 
-// for Windows Users
 const HOSTNAME = 'localhost';
 // const HOSTNAME = process.env.HOSTNAME ?? 'localhost';
 const PORT = Number(process.env.PORT) ?? 4000;
@@ -23,33 +22,10 @@ const app: Express = express();
 
 /* Middleware --------------------------------------------------------------- */
 
-// app.use(greetingMessage);
 app.use(express.static(resolve(__dirname, '../public')));
 app.use(express.json());
 
 /* Routing ------------------------------------------------------------------ */
-//
-// 라우팅은 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 어떻게 응답할지 결정하는 것을 말하며,
-// 이는 URI(또는 경로)와 특정 HTTP 요청 메서드(GET, POST, PUT, PATCH, DELETE 등)입니다.
-// 각 경로에는 하나 이상의 핸들러 함수가 있을 수 있으며, 이 함수는 경로가 일치할 때 실행됩니다.
-//
-// --------------------------------------------------------------------------
-
-// GET
-// app.get('/', entryHandler);
-
-// POST
-// app.post('/', (request, response) => {
-//   // 클라이언트 요청 URL
-//   console.log(request.url);
-
-//   // 서버 -> 클라이언트 응답
-//   response.status(201 /* Created */).send({
-//     message: 'POST 요청이 홈페이지로부터 주어졌습니다.',
-//   });
-// });
-
-/* API ---------------------------------------------------------------------- */
 
 app.use('/api/users', usersRouter);
 
